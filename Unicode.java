9eeaea634afacbf02abf478c00d9aad526c57ff5
jack200210 <43202744+jack200210@users.noreply.github.com>
2018-12-22 13:27:33 +0300

    Create Unicode.java

---
+++ Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode {
+    public static void main (String [] args) {
+
+    	for (char symbolNumber = 33; symbolNumber <= 126; symbolNumber++) {
+        	System.out.println(symbolNumber);
+    	}
+    }
+}
